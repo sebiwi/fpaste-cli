4a01a81983c3550363b0d8c5969fbf8cbfb51d8c
Sebastian Caceres <sebastian.caceresb@gmail.com>
2017-11-03 12:55:45 +0100

    Update to match new fpaste api

--- fpaste.py
+++ fpaste.py
@@ -5,32 +5,31 @@ import requests
 from subprocess import Popen, PIPE
 import sys
 
-class FPaste:
 
-    fpaste_api = "http://www.fpaste.org/"
-    
+class FPaste:
+    fpaste_api = "https://paste.fedoraproject.org/api/paste/submit"
+
     def __init__(self):
         self.data = []
-    
+
     def post_code(self, paste_data):
-        payload = (('paste_lang', 'text'), ('mode', 'json'), ('paste_data', paste_data), ('api_submit', 'true'))
-        request = requests.get(self.fpaste_api, params=payload)
-        return self.fpaste_api + request.json()['result']['id']
+        request = requests.post(self.fpaste_api, json={'contents': paste_data})
+        return request.json()['url']
+
 
 def copy_text(text):
-    
     cb_name = get_clipboard_name()
-    
+
     if cb_name is not None:
         clipboard = Popen(cb_name, shell=True, stdin=PIPE).stdin
         clipboard.write(text)
         clipboard.close()
 
+
 def get_clipboard_name():
-
     cb_list = ['pbcopy', 'xclip', 'putclip']
-
     cb_name = None
+
     for cb in cb_list:
         if cli_exists(cb):
             cb_name = cb
@@ -40,31 +39,31 @@ def get_clipboard_name():
 
 
 def cli_exists(command):
-
     exists = False
     test = 'type %s >> /dev/null 2>&1' % command
     process = Popen(test, shell=True, stdout=PIPE)
     process.stdout.close()
+
     if not process.wait():
         exists = True
 
     return exists
 
+
 def main():
-    
     parser = argparse.ArgumentParser()
     parser.add_argument("-f","--filename",help="Upload the specified file, instead of stdin")
     args = parser.parse_args()
- 
+
     if args.filename is not None:
         code = open(args.filename, 'r')
     else:
         code = sys.stdin
 
     fpaste = FPaste()
-    
     copy_text(fpaste.post_code(code.read()))
     code.close()
-        
-if __name__ == "__main__": 
+
+
+if __name__ == "__main__":
     main()
